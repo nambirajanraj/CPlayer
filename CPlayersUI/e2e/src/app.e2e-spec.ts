45b5a830a2319c1e89edf7d0642132e55c21c432
Ubuntu <ubuntu@ip-172-31-16-111.ec2.internal>
2019-04-27 11:17:13 +0000

    e2e and docker

--- CPlayersUI/e2e/src/app.e2e-spec.ts
+++ CPlayersUI/e2e/src/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { browser, by, element, protractor } from 'protractor';
 import { async } from 'q';
 
 
-describe('MovieCruiser-frontend App', () => {
+describe('CPlayerUI App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -23,23 +23,23 @@ describe('MovieCruiser-frontend App', () => {
   });
 
   it('should be redirected to /register route', () => {
-    browser.element(by.css('.register-user')).click();
+    browser.element(by.css('.register-button')).click();
     expect(browser.getCurrentUrl()).toContain('/register');
   });
 
   it('should be able to register user', () => {
     browser.driver.sleep(1000);
-    browser.element(by.id('firstName')).sendKeys('Super User');
-    browser.element(by.id('lastName')).sendKeys('Super lastUser');
-    browser.element(by.id('userId')).sendKeys('Super User12');
-    browser.element(by.id('password')).sendKeys('Super Userpass');
+    browser.element(by.id('firstName')).sendKeys('Nambi');
+    browser.element(by.id('lastName')).sendKeys('Rajan');
+    browser.element(by.id('userId')).sendKeys('Nambi1234');
+    browser.element(by.id('password')).sendKeys('pass1234');
     browser.element(by.css('.register-user')).click();
     expect(browser.getCurrentUrl()).toContain('/login');
   });
 
   it('should be able to login user and navigate to search players', () => {
-    browser.element(by.id('userId')).sendKeys('Super User12');
-    browser.element(by.id('password')).sendKeys('Super Userpass');
+    browser.element(by.id('userId')).sendKeys('Nambi1234');
+    browser.element(by.id('password')).sendKeys('pass1234');
     browser.element(by.css('.login-user')).click();
     expect(browser.getCurrentUrl()).toContain('/players/search');
   });
